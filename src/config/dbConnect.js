5a58d9ecd92a2189bf116ad569c686581415d8e8
kamilajesus <kamilajesusdev@gmail.com>
2022-11-09 22:34:57 -0300

    Finalizado

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose"
 
-mongoose.connect("mongodb+srv://Kamila:10190207@cluster0.pqpc21b.mongodb.net/alura-livros")
+mongoose.connect("mongodb+srv://Kamila:10190207@cluster0.pqpc21b.mongodb.net/?retryWrites=true&w=majority");
 
 let db = mongoose.connection;
 
